98b1b3a121f0aba1873d0a893a50295aaea871b2
Bipin Mathew <bipinmathew@gmail.com>
2016-07-25 19:26:50 -0400

    unsigned sort now works.

--- b32sort.c
+++ b32sort.c
@@ -9,14 +9,14 @@
 #define _2(x) ((x >> 16) & 0x000000FF)
 #define _3(x) ((x >> 24) & 0x000000FF)
 
-int validate_sort(const uint32_t *a, const unsigned int *p, unsigned int N){
+int validate_sort(const int32_t *a, const unsigned int *p, unsigned int N){
   unsigned int i;
 
   for(i=1;i<N;i++){
     if(a[p[i]]<a[p[i-1]]){
       printf("Invalid permutation value at index %u\n",i);
       printf("  p[i] = %u, p[i-1]=%u\n",p[i],p[i-1]);
-      printf("  a[p[i]] = %u, a[p[i-1]]=%u\n",a[p[i]],a[p[i-1]]);
+      printf("  a[p[i]] = %d, a[p[i-1]]=%d\n",a[p[i]],a[p[i-1]]);
 
       printf("I: ");
       for(i=0;i<N;i++){
@@ -26,13 +26,13 @@ int validate_sort(const uint32_t *a, const unsigned int *p, unsigned int N){
 
       printf("a[i]: ");
       for(i=0;i<N;i++){
-        printf("%u ",a[i]);
+        printf("%d ",a[i]);
       }
       printf("\n\n");
 
       printf("a[p[i]]: ");
       for(i=0;i<N;i++){
-        printf("%u ",a[p[i]]);
+        printf("%d ",a[p[i]]);
       }
       printf("\n\n");
       
@@ -44,19 +44,19 @@ int validate_sort(const uint32_t *a, const unsigned int *p, unsigned int N){
   return(0);
 } 
 
-int bu32sort(const uint32_t *a, unsigned int **p, unsigned int N){
+int bu32sort(const int32_t *a, unsigned int **p, unsigned int N){
   unsigned int i,j;
   unsigned int d;
   static const unsigned int mask = 0x000000FF;
   unsigned int B[4*256],*C;
-  uint32_t c,*buff;
+  int32_t c,*buff;
   unsigned int *iwriter,rank,*I;
   const unsigned int *ireader;
-  const uint32_t *reader;
-  uint32_t *writer;
+  const int32_t *reader;
+  int32_t *writer;
 
 
-  if((buff=malloc(2*N*sizeof(uint32_t)))==NULL){
+  if((buff=malloc(2*N*sizeof(int32_t)))==NULL){
     return(1);
   }
 
@@ -78,7 +78,7 @@ int bu32sort(const uint32_t *a, unsigned int **p, unsigned int N){
   writer=&buff[N];
 
   memset(B,0,4*256*sizeof(unsigned int));
-  for(rank=0;rank<=3;rank++){
+  for(rank=0;rank<=2;rank++){
     C=&B[256*rank];
     d = rank*8;
 
@@ -111,6 +111,43 @@ int bu32sort(const uint32_t *a, unsigned int **p, unsigned int N){
   }
 
 
+
+  rank=3;
+
+  C=&B[256*rank];
+  d = rank*8;
+
+
+  for(i=0;i<N;i++){
+    c = (((unsigned)reader[i]) >> d) ^ 0x00000080;
+    C[c]+=1;
+  }
+
+  for(i=1;i<256;i++){
+    C[i]=C[i]+C[i-1];
+  }
+
+  for(j=1;j<=N;j++){
+    i=N-j;
+    c = (((unsigned)reader[i]) >> d) ^ 0x00000080;
+    iwriter[C[c]-1]=ireader[i];
+    writer[C[c]-1] = reader[i];
+    C[c]-=1;
+  }
+
+
+  iwriter=&I[((rank)%2)*N];
+  ireader=&I[((rank+1)%2)*N];
+
+  writer=&buff[((rank)%2)*N];
+  reader=&buff[((rank+1)%2)*N];
+
+
+
+
+
+
+
   if((*p=realloc(*p,sizeof(unsigned int)*N))==NULL){
     return(1);
   }
@@ -120,7 +157,7 @@ int bu32sort(const uint32_t *a, unsigned int **p, unsigned int N){
 }
 
 int main(){
-  uint32_t *a;
+  int32_t *a;
   unsigned int N,M,i,*I,n,m;
   clock_t begin,end;
   int e;
@@ -133,19 +170,15 @@ int main(){
     M=1;
     for(m=0;m<9;m++){
         M*=10;
-        if((a = malloc(sizeof(uint32_t)*N))==NULL){
+        if((a = malloc(sizeof(int32_t)*N))==NULL){
           printf("malloc failed...\n");
           exit(1);
         }
         //I = (unsigned int *)malloc(sizeof(unsigned int)*N);
         for(i=0;i<N;i++){
-          a[i] = rand()%M;
+          a[i] = (rand()%(2*M))-M;
         }
 
-        // for(i=0;i<N;i++){
-        //   printf("%u ",a[i]);
-        // }
-        // printf("\n");
 
         begin=clock();
         e=bu32sort(a,&I,N);
@@ -158,10 +191,10 @@ int main(){
         }
 
         if(validate_sort(a,I,N)!=0){
-          printf("** Invalid sort ** N: %u, M: %u,time: %f, uint32_t/sec: %f\n",N,M,diff,N/diff);
+          printf("** Invalid sort ** N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
           exit(1);
         }
-        printf("N: %u, M: %u,time: %f, uint32_t/sec: %f\n",N,M,diff,N/diff);
+        printf("N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
         free(a);
         free(I);
     }
