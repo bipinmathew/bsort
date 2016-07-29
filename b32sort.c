5f350a9883e60b138aac9ced2858ce378a85908c
Bipin Mathew <bipinmathew@gmail.com>
2016-07-29 13:33:35 -0400

    Adding commandling logic.

--- b32sort.c
+++ b32sort.c
@@ -233,49 +233,87 @@ int b32sort(const int32_t *a, unsigned int **p, unsigned int N){
   return(0);
 }
 
-int main(){
+int main(int argc, char **argv){
   int32_t *a;
   unsigned int N,M,i,*I,n,m;
   clock_t begin,end;
   int e;
   double diff;
 
+  if(argc<3){
+    N=1;
+    for(n=0;n<8;n++){
+      N*=10;
+      M=1;
+      for(m=0;m<9;m++){
+          M*=10;
+          if((a = malloc(sizeof(int32_t)*N))==NULL){
+            printf("malloc failed...\n");
+            exit(1);
+          }
+          //I = (unsigned int *)malloc(sizeof(unsigned int)*N);
+          for(i=0;i<N;i++){
+            a[i] = (rand()%(2*M))-M;
+          }
 
-  N=1;
-  for(n=0;n<8;n++){
-    N*=10;
-    M=1;
-    for(m=0;m<9;m++){
-        M*=10;
-        if((a = malloc(sizeof(int32_t)*N))==NULL){
-          printf("malloc failed...\n");
-          exit(1);
-        }
-        //I = (unsigned int *)malloc(sizeof(unsigned int)*N);
-        for(i=0;i<N;i++){
-          a[i] = (rand()%(2*M))-M;
-        }
 
+          begin=clock();
+          e=b32sort(a,&I,N);
+          end=clock();
+          diff=(double)(end - begin) / CLOCKS_PER_SEC;
 
-        begin=clock();
-        e=b32sort(a,&I,N);
-        end=clock();
-        diff=(double)(end - begin) / CLOCKS_PER_SEC;
+          if(e>0){
+            printf("  there appears to have been a memory allocation error.\n");
+            exit(1);
+          }
 
-        if(e>0){
-          printf("  there appears to have been a memory allocation error.\n");
-          exit(1);
-        }
-
-        if(validate_sort(a,I,N)!=0){
-          printf("** Invalid sort ** N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
-          exit(1);
-        }
-        printf("N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
-        free(a);
-        free(I);
+          if(validate_sort(a,I,N)!=0){
+            printf("** Invalid sort ** N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
+            exit(1);
+          }
+          printf("N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
+          free(a);
+          free(I);
+      }
     }
   }
+  else{
+    N=atoi(argv[1]);
+    M=atoi(argv[2]);
+
+
+    if((a = malloc(sizeof(int32_t)*N))==NULL){
+      printf("malloc failed...\n");
+      exit(1);
+    }
+    //I = (unsigned int *)malloc(sizeof(unsigned int)*N);
+    for(i=0;i<N;i++){
+      a[i] = (rand()%(2*M))-M;
+    }
+
+
+    begin=clock();
+    e=b32sort(a,&I,N);
+    end=clock();
+    diff=(double)(end - begin) / CLOCKS_PER_SEC;
+
+    if(e>0){
+      printf("  there appears to have been a memory allocation error.\n");
+      exit(1);
+    }
+
+    if(validate_sort(a,I,N)!=0){
+      printf("** Invalid sort ** N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
+      exit(1);
+    }
+    printf("N: %u, M: %u,time: %f, int32_t/sec: %f\n",N,M,diff,N/diff);
+    free(a);
+    free(I);
+
+
+  }
+
+
   return(0);
 
 }
